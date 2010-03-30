98d18b9559c99a1e914031e77fe56b87909d671e
Tom Wieland <tom.wieland@gmail.com>
2010-03-30 20:57:28 +0200

    make license public domain rename a var

--- Addon.lua
+++ Addon.lua
@@ -1,32 +1,3 @@
---[[----------------------------------------------------------------------------
-  Copyright (c) 2009, Tom Wieland
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-
-  * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-  * Neither the name of idChat nor the names of its contributors may be used
-    to endorse or promote products derived from this software without specific
-    prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  POSSIBILITY OF SUCH DAMAGE.
-------------------------------------------------------------------------------]]
-
 local _G = _G
 
 local TL, TC, TR = 'TOPLEFT', 'TOP', 'TOPRIGHT'
@@ -61,7 +32,7 @@ local function hideFrameForever (f)
 	f:Hide()
 end
 
-local function AddMessage(frame, text, red, green, blue, id)
+local function AddMessage(frame, text, red, green, blue, alpha)
 	text = tostring(text) or ''
 
 	-- channels
@@ -85,7 +56,7 @@ local function AddMessage(frame, text, red, green, blue, id)
 	-- timestamp
 	text = '|cff999999' .. date('%H%M') .. '|r ' .. text
 
-	return hooks[frame](frame, text, red, green, blue, id)
+	return hooks[frame](frame, text, red, green, blue, alpha)
 end
 
 local function scrollChat(frame, delta)
